51556be63a3444217a3cca5cf5621db3e0af0da4
winerss <2857505905@qq.com>
2018-11-25 18:25:06 +0800

    20181125

---
+++ src/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="index">
+    <button type="primary" size="mini">提交</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  components: {},
+
+  methods: {},
+
+  created() {}
+};
+</script>
+
+<style>
+</style>
